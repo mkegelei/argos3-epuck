f2c6337bca287ab9f7b48b1530a2710aa6b9e562
mkegelei <mkegelei@ulb.ac.be>
2018-04-25 16:30:33 +0200

    patch yocto

--- src/cmake/ARGoSBuildFlags.cmake
+++ src/cmake/ARGoSBuildFlags.cmake
@@ -9,12 +9,12 @@ execute_process(
 #
 # General compilation flags
 #
-set(CMAKE_C_FLAGS                "-Wall")
+set(CMAKE_C_FLAGS                "${CMAKE_C_FLAGS} -Wall")
 set(CMAKE_C_FLAGS_RELEASE        "-Os -DNDEBUG")
 set(CMAKE_C_FLAGS_RELWITHDEBINFO "-Os -ggdb3 -DNDEBUG")
 set(CMAKE_C_FLAGS_DEBUG          "-ggdb3")
 
-set(CMAKE_CXX_FLAGS                "-Wall")
+set(CMAKE_CXX_FLAGS                "${CMAKE_CXX_FLAGS} -Wall -std=c++11")
 set(CMAKE_CXX_FLAGS_RELEASE        "-Os -DNDEBUG")
 set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-Os -ggdb3 -DNDEBUG")
 set(CMAKE_CXX_FLAGS_DEBUG          "-ggdb3")
